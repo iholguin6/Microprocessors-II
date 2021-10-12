c201c2e108cd8bc7a73e8f63f3cb7b6c7f8ba312
Ismael Holguin <iholguin6@miners.utep.edu>
2021-10-11 21:21:07 -0600

    added Lab_3

---
+++ Lab_3/main/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+int main(){
+    
+}
+//to build on terminal input: get_idf
+//idf.py -p /dev/ttyS3 -b 115200 flash monitor
+// cp -r sample_project Labx
